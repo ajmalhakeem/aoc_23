5d18006113d498cd846375ab7051c89faeda6dd3
hkmjtech <ajmlhkm@gmail.com>
2023-12-02 13:34:07 +0800

    handle edge case

--- d1/d1p2.rb
+++ d1/d1p2.rb
@@ -2,9 +2,16 @@ def read_file file_name
     file_data = File.open(file_name).readlines.map(&:chomp)
 end
 
+def convert_to_num hash,map
+    if hash.keys.first.match?(/\A\d+\z/)
+        hash.keys.first
+    else 
+        map[hash.keys.first]
+    end
+end
 
-def convert_line line
-    word_number_lookup = {
+def convert_words words
+    word_number_map = {
         "one" => "1",
         "two" => "2",
         "three" => "3",
@@ -16,17 +23,33 @@ def convert_line line
         "nine" => "9",
         "zero" => "0"
     }
-
-    formatted_line = line
-    
-    word_number_lookup.each do |word, number| 
-        if line.include? word
-            formatted_line = formatted_line.sub word, number
+   
+    sum = 0
+    words.map do |word|
+        words_found = []
+        digits_found = []
+        word_number_map.each do |key, value|
+            if word.include? key
+                words_found.append({key => word.rindex(key)})
+            end
+            if word.include? value
+                digits_found.append({value => word.rindex(value)})
+            end
         end
+        
+        combined_found = words_found + digits_found
+        first_number = combined_found.min_by { |h| h.values.first }
+        last_number = combined_found.max_by {|h| h.values.first}
+        
+        combined_num = convert_to_num(first_number, word_number_map) + convert_to_num(last_number, word_number_map) 
+   
+        sum += combined_num.to_i
+
+        sum
     end
-    return formatted_line
+
+    sum
+
 end
 
-read_file("samplep2.txt").each do |line| 
-    puts convert_line(line)
-end
+puts convert_words(read_file("input.txt"))
